22d682980344bf680744ddb177e512cb69e4b323
lolito <prof.lolito@gmail.com>
2024-01-09 16:09:57 +0200

    FE BE Merge Start

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 function Register() {
 
@@ -8,23 +9,41 @@ function Register() {
         email: '',
         password1: '',
         password2: ''
-    })
+    });
+
+    const [error, setError] = useState('');
+    const navigate = useNavigate;
 
     const changeInputHandler = (e) => {
         setUserData(prevState => {
             return {...prevState, [e.target.name] : e.target.value}
         })
     }
+
+    const registerUser = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/users/register`, userData)
+            const newUser = await response.data;
+            console.log(newUser);
+            if(!newUser) {
+                setError('Could not register user. Please try again.');
+            }
+            navigate('/login');
+        } catch (error) {
+            setError(error.response.data.message)
+        }
+    }
     return (
         <div>
             <section className="register">
                 <div className="container">
                     <h2>Sign Up</h2>
-                    <form action="" className="form register__form">
-                         <p className="form__error-message">Error</p>
+                    <form action="" className="form register__form" onSubmit={ registerUser }>
+                         {error && <p className="form__error-message">{ error }</p> }
                          <input type="text" placeholder='Full Name' name='name' value={userData.name} onChange={changeInputHandler}/>
                          <input type="email" placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler}/>
-                         <input type="password" placeholder='Password' name='password1' value={userData.password1} onChange={changeInputHandler}/>
+                         <input type="password" placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler}/>
                          <input type="password" placeholder='Confirm Password' name='password2' value={userData.password2} onChange={changeInputHandler}/>
                          <button type='submit' className="btn primary">Register</button>
                     </form>
