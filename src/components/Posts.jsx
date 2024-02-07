ab9857889a3649694fd96a5a4403e53b775fdcf6
lolito <prof.lolito@gmail.com>
2024-02-07 11:49:00 +0200

    Pagination

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -6,36 +6,68 @@ import axios from 'axios';
 function Posts() {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const fetchPosts = async () => {
       setIsLoading(true);
       try {
-        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts`);
-        setPosts(response?.data)
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts?page=${currentPage}`);
+        setPosts(response?.data.posts);
+        setTotalPages(Math.ceil(response?.data.total / 10)); // Assuming page_size is always 10
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
       setIsLoading(false);
-    }
-    fetchPosts();
-  }, []);
+    };
 
-  if(isLoading) {
-    return <Loader />
+    fetchPosts();
+  }, [currentPage]);
+
+  const handlePrevious = () => {
+    setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : prevPage));
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prevPage) => (prevPage < totalPages ? prevPage + 1 : prevPage));
+  };
+
+  if (isLoading) {
+    return <Loader />;
   }
 
   return (
     <section className="posts">
-       {posts.length > 0 ? <div className="container posts__container">
-        {
-          posts.map(({_id: id, thumbnail, category, title, content, author, createdAt}) => 
-          <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title}
-          content={content} authorID={author} createdAt={createdAt}/>)
-        }
-       </div> : <h2 className='center'>No Posts Published</h2> }
+      {posts.length > 0 ? (
+        <div className="container posts__container">
+          {posts.map(({ _id: id, thumbnail, category, title, content, author, createdAt }) => (
+            <PostItem
+              key={id}
+              postID={id}
+              thumbnail={thumbnail}
+              category={category}
+              title={title}
+              content={content}
+              authorID={author}
+              createdAt={createdAt}
+            />
+          ))}
+        </div>
+      ) : (
+        <h2 className="center">No Posts Published</h2>
+      )}
+      <div className="pagination">
+        <button disabled={currentPage === 1} onClick={handlePrevious}>
+          Previous
+        </button>
+        <span>Page {currentPage} of {totalPages}</span>
+        <button disabled={currentPage === totalPages} onClick={handleNext}>
+          Next
+        </button>
+      </div>
     </section>
-    )
+  );
 }
 
-export default Posts
+export default Posts;
