61774d913d89dc95a47b76d1c48c23609e534dad
radlee <prof.lolito@gmail.com>
2024-02-08 00:00:10 +0200

    Pagination Styles

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -57,28 +57,28 @@ function Posts() {
       ) : (
         <h2 className="center">No Posts Published</h2>
       )}
-      <div className="pagination">
-        
+        <div className="pagination">
         <button disabled={currentPage === 1} onClick={handlePrevious}>
           Previous
         </button>
-          {/* Additional pagination options on the left */}
-          {Array.from({ length: Math.min(currentPage - 1, 4) }, (_, i) => (
+        {/* Additional pagination options on the left */}
+        {Array.from({ length: Math.min(currentPage - 1, 4) }, (_, i) => (
           <button key={i} onClick={() => setCurrentPage(currentPage - i - 1)}>
             {currentPage - i - 1}
           </button>
         ))}
-        <span>Page {currentPage} of {totalPages}</span>
-        <button disabled={currentPage === totalPages} onClick={handleNext}>
-          Next
-        </button>
-      
+        <div className='pageteller'>
+          <button id='counter'>{currentPage} of {totalPages}</button>
+        </div>
         {/* Additional pagination options on the right */}
         {Array.from({ length: Math.min(totalPages - currentPage, 4) }, (_, i) => (
           <button key={i} onClick={() => setCurrentPage(currentPage + i + 1)}>
             {currentPage + i + 1}
           </button>
         ))}
+        <button disabled={currentPage === totalPages} onClick={handleNext}>
+          Next
+        </button>
       </div>
     </section>
   );
