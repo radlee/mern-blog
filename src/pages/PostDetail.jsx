5cfecd6272e07bea0479be12bd53b9aeeb721316
lolito <prof.lolito@gmail.com>
2024-02-12 16:41:18 +0200

    Disqus

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -3,70 +3,72 @@ import PostAuthor from '../components/PostAuthor';
 import { Link, useParams } from 'react-router-dom';
 import Loader from '../components/Loader';
 import DeletePost from './DeletePost';
-import Disqus from "disqus-react"
+import Disqus from 'disqus-react';
 import { UserContext } from '../context/userContext';
 import axios from 'axios';
 
-
 function PostDetail() {
-    
-    const { currentUser } = useContext(UserContext);
-    // const { cu_id } = currentUser.id;
-    const { id } = useParams();
-    console.log("Params id : ", id)
-    // console.log("Current User id : ", cu_id)
-    const [post, setPost] = useState(null);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
+  const { currentUser } = useContext(UserContext);
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const getPost = async () => {
-            setIsLoading(true);
-            try {
-                const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts/${id}`);
-                setPost(response.data);
-            } catch (error) {
-                setError(error);
-            }
-            setIsLoading(false);
-        }
-        getPost();
-    }, []);
-
-    if(isLoading) {
-        return <Loader />
-    }
-    const disqusShortname = "radblokmultimedia";
-
-    const disqusConfig = {
-        url: "https://radblokmultimedia.onrender.com",
-        identifier: `${id}`,
-        title: `${post.title}`
+  useEffect(() => {
+    const getPost = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts/${id}`);
+        setPost(response.data);
+      } catch (error) {
+        setError(error);
       }
+      setIsLoading(false);
+    };
+    getPost();
+  }, [id]);
 
-    return (
-        <section className="post-detail">
-            {error && <p className='error'>{error}</p>}
-            {post && <div className="container post-detail__container">
-                <div className="post-detail__header">
-                    <PostAuthor authorID={post.author} createdAt={post.createdAt}/>
-                    {currentUser?.id === post?.author && <div className="post-detail__buttons">
-                        <Link to={`/posts/${post?._id}/edit`} className='btn sm primary'>Edit</Link>
-                        <DeletePost postId={id} />
-                    </div>}
-                </div>
-                <h1>{post.title}</h1>
-                <div className="post-detail__thumbnail">
-                    <img src={post.thumbnail} alt="Thumbnail" />
-                </div>
-                <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
-            </div> }
-            <Disqus.DiscussionEmbed
-          shortname={disqusShortname}
-          config={disqusConfig}
-        />
-        </section>
-    )
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p className="error">{error.message}</p>;
+  }
+
+  const disqusShortname = 'radblokmultimedia';
+
+  const disqusConfig = {
+    url: `https://radblokmultimedia.onrender.com/posts/${id}`, // Adjust the URL
+    identifier: `${id}`,
+    title: post ? post.title : '', // Check if post is not null before accessing properties
+  };
+
+  return (
+    <section className="post-detail">
+      {post && (
+        <div className="container post-detail__container">
+          <div className="post-detail__header">
+            <PostAuthor authorID={post.author} createdAt={post.createdAt} />
+            {currentUser?.id === post?.author && (
+              <div className="post-detail__buttons">
+                <Link to={`/posts/${post?._id}/edit`} className="btn sm primary">
+                  Edit
+                </Link>
+                <DeletePost postId={id} />
+              </div>
+            )}
+          </div>
+          <h1>{post.title}</h1>
+          <div className="post-detail__thumbnail">
+            <img src={post.thumbnail} alt="Thumbnail" />
+          </div>
+          <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
+        </div>
+      )}
+      <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+    </section>
+  );
 }
 
-export default PostDetail
+export default PostDetail;
