896863ce466a524c7cc2a011409c5e888eb73238
lolito <prof.lolito@gmail.com>
2024-02-19 14:19:08 +0200

    Quill Code and Colors

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -29,9 +29,12 @@ function CreatePost() {
   const modules = {
     toolbar: [
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block','formula'],
       [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
       ['link', 'image', 'video'],
+      [{ 'color': [] }, { 'background': [] }],    
+      [{ 'font': [] }],
+      [{ 'align': [] }],
       ['clean'],
     ],
   };
--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -34,7 +34,7 @@ function EditPost() {
   const modules = {
     toolbar: [
       [{'header': [1, 2, 3, 4, 5, 6, false]}],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block', 'formula'],
       [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
       ['link', 'image', 'video'],
       ['clean'],
