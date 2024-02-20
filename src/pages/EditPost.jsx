90a9367eb526f96b26b95ab124b47e441514815f
lolito <prof.lolito@gmail.com>
2024-02-20 11:05:46 +0200

    Quill Styles AND iNDENTS

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -33,12 +33,20 @@ function EditPost() {
 
   const modules = {
     toolbar: [
-      [{'header': [1, 2, 3, 4, 5, 6, false]}],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block', 'formula'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image', 'video'],
-      ['clean'],
-      ['color', 'background', 'script', 'font', 'size', 'align', 'direction', 'code', 'formula']
+      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+      ['blockquote', 'code-block'],
+      ['link', 'image', 'video', 'formula'],
+      [{ 'header': 1 }, { 'header': 2 }], // custom button values
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+      [{ 'script': 'sub' }, { 'script': 'super' }], // superscript/subscript
+      [{ 'indent': '-1' }, { 'indent': '+1' }], // outdent/indent
+      [{ 'direction': 'rtl' }], // text direction
+      [{ 'size': ['small', false, 'large', 'huge'] }], // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }, { 'background': [] }], // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+      ['clean'] // remove formatting button
     ]
   }
 
@@ -49,7 +57,7 @@ function EditPost() {
     'link', 'image', 'video',
     'color', 'background', 'script', 'font', 'size', 'align', 'direction',
     'code', 'formula', 'clean'
-  ]
+  ];
 
   const POST_CATEGORIES = ['General','Health and Wellness', 'Technology', 'Personal Development', 'Finance', 'Career and Business', 'Travel', 'Lifestyle','Food and Cooking']
 
